43b14490fd7f7eb7625a8c2a497b745e97b45d65
Pranjal Singh <65503194+pranjiz@users.noreply.github.com>
2023-08-08 15:40:12 +0530

    Create 4-Find Area of Rectangle.cpp

---
+++ Create 4-Find Area of Rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int L,B,area;
+    cin >> L;
+    cin >> B;
+    area = L*B;
+    cout << area;
+    return 0;
+}
+
+// Declare three integer variables: L, B, and area.
+// user to input the length of the rectangle using cin >> L;. The user to enter the length on the console, and the program will store it in the variable L.
+// the user to input the width of the rectangle using cin >> B;. The user to enter the width on the console, and the program will store it in the variable B.
+// Calculate the area of the rectangle by multiplying the length (L) and the width (B) and store the result in the variable area.
+// Output the calculated area using cout << area;. The program will display the calculated area of the rectangle on the console.
+// Terminate the program by returning 0 from the main function.
