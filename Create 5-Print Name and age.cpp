360e63a1e23dab71af3a1f0bc30eddf5ef94995e
Pranjal Singh <65503194+pranjiz@users.noreply.github.com>
2023-08-08 15:48:51 +0530

    Create 5-Print Name and age.cpp

---
+++ Create 5-Print Name and age.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    string name;
+    cin>>name;
+    int age;
+    cin>>age;
+    cout<<"The name of the person is "<<name<<" and the age is "<<age<<"."<<endl;
+    return 0;
+}
+
+// Declare a string variable name to store the name of the person.
+// user to input the name using cin >> name;. The user is expected to enter the name on the console, and the program will store it in the name variable.
+// Declare an int variable age to store the age of the person.
+// Prompt the user to input the age using cin >> age;. The user is expected to enter the age on the console, and the program will store it in the age variable.
+// Output the formatted message using cout << .... The program will display the name and age of the person in a sentence.
+// Terminate the program by returning 0 from the main function.
